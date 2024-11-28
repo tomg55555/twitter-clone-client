3221d85df55c32cb573aab12941d4427cf5ebf44
tomg55555 <tommasogarzaro@gmail.com>
2024-11-28 18:50:00 +0100

    fix: linting & typing errors

--- components/comment.tsx
+++ components/comment.tsx
@@ -47,8 +47,8 @@ export function Comment( { postId } : CommentPage ){
 
         growers.forEach((grower) => {
             const textarea = grower.querySelector("textarea");
-            textarea.addEventListener("input", () => {
-                //@ts-expect-error:later
+            //@ts-expect-error:later
+            textarea?.addEventListener("input", () => {
                 grower.dataset.replicatedValue = textarea.value;
             });
         });
